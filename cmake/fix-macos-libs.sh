daf356bd51fc304e9c81219a36014236a94103ed
Egil <egil.moller@freecode.no>
2025-01-14 19:00:51 +0100

    Added debug print to macos fix script

--- cmake/fix-macos-libs.sh
+++ cmake/fix-macos-libs.sh
@@ -7,16 +7,17 @@
 readonly RUNTIME_PATH="@executable_path/../Frameworks/"
 
 echo "Fixing maxos library paths in $(pwd)..."
-
-plugin=$(find . -maxdepth 1 -name '*.dylib')
-for lib in $(otool -L "$plugin" | awk ' /wx/ {print $1}'); do
-    libdir=${lib%/*}
-    if [ "$libdir" = "$lib" ]; then
-        continue
-    elif [ "$libdir" != "$RUNTIME_PATH" ]; then
-        runtime_lib=$(echo $lib | sed "s|$libdir|$RUNTIME_PATH|")
-        install_name_tool -change "$lib" "$runtime_lib" "$plugin"
-    fi
+find . -name '*.dylib' | while read plugin; do
+    echo "    Fixing library $plugin..."
+    for lib in $(otool -L "$plugin" | awk ' /wx/ {print $1}'); do
+        libdir=${lib%/*}
+        if [ "$libdir" = "$lib" ]; then
+            continue
+        elif [ "$libdir" != "$RUNTIME_PATH" ]; then
+            runtime_lib=$(echo $lib | sed "s|$libdir|$RUNTIME_PATH|")
+            install_name_tool -change "$lib" "$runtime_lib" "$plugin"
+        fi
+    done
+    echo "fix-macos-libs.sh: Revised library paths:"
+    otool -L "$plugin" | grep wx
 done
-echo "fix-macos-libs.sh: Revised library paths:"
-otool -L "$plugin" | grep wx
